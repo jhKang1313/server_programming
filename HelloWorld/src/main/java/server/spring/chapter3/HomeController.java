8231a9a1b0876ef58d0a145936f580bdb7c5bc74
jhKang <jinhuk1313@gmail.com>
2015-12-11 16:25:23 +0900

    artList를 초기화하는 부분, 출력하는 부분으로 나눔

--- HelloWorld/src/main/java/server/spring/chapter3/HomeController.java
+++ HelloWorld/src/main/java/server/spring/chapter3/HomeController.java
@@ -27,21 +27,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	private ArrayList<Article> articles = new ArrayList<Article>();
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	private int noTotalArticles = 0;
+	private int noPapges;
+	private String sError;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = "2015-11-27- Thur. 4:33:59";
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "home";
 	}
 	@RequestMapping(value = "/contents", method = RequestMethod.GET)
@@ -51,9 +53,58 @@ public class HomeController {
 	}
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String artList(Locale locale, Model model, HttpServletRequest request) {	//HttpServletRequest param 추가
-		int noTotalArticles = 0;
+		initListPage() ;
+		showPageX(113) ;
+		model.addAttribute("strError", sError);
+		model.addAttribute("noTotalArticles", noTotalArticles);	//${noTotalArticles}에 값을 준다.
+		model.addAttribute("noPages", noPapges);
+		//model.addAttribute("articlesList", articles); Model로 넘겨주지말고 Request로 넘겨주면 인자를 jsp 파일 안에서 받을 수 있다.
+		request.setAttribute("articleList", articles) ;
+		return "artList";	//보여줄 jsp 파일 이름
+	}
+	public void showPageX(int index){
+		Connection con = null ;
+		articles = new ArrayList<Article>() ;
+		try{
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver"); 
+			String url = "jdbc:ucanaccess://C:/CS-Board2003.mdb";
+			con = DriverManager.getConnection(url); 
+			
+			Statement st = con.createStatement() ;
+	ResultSet rs = st.executeQuery(
+			"select subject,name,created,hit,id from bbs_studypds order by created DESC") ;
+	
+			for(int i=0; i<index*10; i++) rs.next() ;
+			int no = 0 ;
+			while (rs.next()  &&  no<10) {
+				Article art = new Article() ;
+				art.setArticleNumber(noTotalArticles-index*10-no);
+				art.setArticleTitle(rs.getString("subject"));
+				art.setArticleAuthor(rs.getString("name"));
+				art.setKey(rs.getInt("id"));
+				
+				String sDate = "" ;
+				sDate += rs.getObject("created");
+				int ndx = sDate.indexOf("00:");				
+				art.setArticleDate(sDate.substring(0, ndx));
+				
+				art.setHitCount(rs.getInt("hit"));
+				articles.add(art) ;
+				no++ ;
+			}
+			st.close();
+			con.close();
+			System.out.println("well done!!");
+		} 		catch (SQLException e) {
+			System.out.println("Error on sql");
+		}		catch (Exception e) {
+			System.out.println("Error on DB-open ");
+		}
+
+
+	}
+	public void initListPage(){			//초기화 하는 내용 -> 변할 리 업슨ㄴ noTotalArticles 와 noPage는 잘 안바뀐다.
 		Connection con = null;
-		String sError = "Success";
 		int count = 0;
 		try{
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver"); 
@@ -76,75 +127,18 @@ public class HomeController {
 				}
 				count++;
 			}
-			int i = 0 ;
-			for(Article article : articles){
-				article.setArticleNumber(count - i);
-				i++;
-			}
-	
-			noTotalArticles = count;
-			rs.close();
 			st.close();
 			con.close();
-		}
-		catch(SQLException e){
-			sError = "Error on sqlDB";
-		}
-		catch(Exception e){
-			sError = "Error on DB";
-		}
-		int noPapges = (noTotalArticles+9)/ 10;
-		model.addAttribute("strError", sError);
-		model.addAttribute("noTotalArticles", noTotalArticles);	//${noTotalArticles}에 값을 준다.
-		model.addAttribute("noPages", noPapges);
-		//model.addAttribute("articlesList", articles); Model로 넘겨주지말고 Request로 넘겨주면 인자를 jsp 파일 안에서 받을 수 있다.
-		request.setAttribute("articleList", articles) ;
-		return "artList";	//보여줄 jsp 파일 이름
-	}
-	void showPageX(int index){
-		int noTotalArticles = 0;
-		Connection con = null;
-		String sError = "Success";
-		int count = 0;
-		try{
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver"); 
-			String url = "jdbc:ucanaccess://C:/CS-Board2003.mdb";
-			con = DriverManager.getConnection(url);
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("select subject, name, created, hit, id from bbs_studypds");
-			for(int i = 0 ; i < index * 10 ; i++)
-				rs.next();
-			for(int i = 0 ; i < 10 && rs.next() ; i++){
-				String sDate = "";
-				sDate += rs.getObject("created");
-				int ndx = sDate.indexOf("00:");
-				Article article = new Article();
-				article.setKey(rs.getInt("id"));
-				article.setArticleTitle(rs.getString("subject"));
-				article.setArticleAuthor(rs.getString("name"));
-				article.setArticleDate(sDate.substring(0, ndx));
-				article.setHitCount(rs.getInt("hit"));
-				articles.add(article); 
-				count++;
-			}
-			int i = 0 ;
-			for(Article article : articles){
-				article.setArticleNumber(count - i);
-				i++;
-			}
-	
-			noTotalArticles = count;
 			rs.close();
-			st.close();
-			con.close();
-		}
-		catch(SQLException e){
-			sError = "Error on sqlDB";
-		}
-		catch(Exception e){
-			sError = "Error on DB";
-		}
 
-		
+			noTotalArticles = count;
+			noPapges = (count + 9)/10;
+		}
+		catch(SQLException e){
+			sError = "Error on sqlDB";
+		}
+		catch(Exception e){
+			sError = "Error on DB";
+		}
 	}
 }
